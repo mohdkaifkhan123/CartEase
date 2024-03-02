a379b886bdde7a851cbdeca024552465a9c4c5d5
mohdkaifkhan123 <khank11111999@gmail.com>
2024-03-02 14:57:04 +0530

    Added router

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import { ProductList } from './features/product-list/ProductList';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import Signup from './features/auth/components/Signup';
+import SignupPage from './pages/SignupPage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+]);
 function App() {
   return (
     <div className="App">
-     <Home/>
+<RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,8 @@
+
+import Login from "../features/auth/components/Login"
+function LoginPage(){
+return(
+    <Login></Login>
+)
+}
+export default LoginPage
